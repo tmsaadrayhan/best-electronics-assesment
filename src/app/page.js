2f0f50faa39c5a36ac2457fb54981adbcc9296d2
tmsaadrayhan <tmsaadrayhan@gmail.com>
2024-11-19 11:33:04 +0600

    change margin

--- src/app/page.js
+++ src/app/page.js
@@ -119,7 +119,7 @@ const page = () => {
         data: [92000, 60000, 55000, 65000, 60000, 90000], // Example order data
         backgroundColor: "#ff7043", // Orange color for bars
         borderRadius: 0, // Rounded corners
-        barThickness: 23,
+        barThickness: 20,
       },
     ],
   };
@@ -179,7 +179,7 @@ const page = () => {
       x: {
         ticks: {
           callback: (value) => `${value.toLocaleString()}৳`, // Add currency symbol
-          font: { size: 18 },
+          font: { size: 12 },
           stepSize: 50000,
           color: "#333",
         },
@@ -200,17 +200,17 @@ const page = () => {
       className="flex bg-[#F8F8F8]"
     >
       {/* nav */}
-      <div className="w-[365px] bg-[#FFFFFF]">
+      <div className="w-[300px] bg-[#FFFFFF]">
         <div className="bg-[#F15B25] w-full">
           <Image
-            className="mx-auto py-[1rem] pointer"
+            className="mx-auto w-[200px] py-[1rem] pointer"
             src={onnow}
             alt="onnow"
           />
         </div>
-        <div className="mt-[3.5rem] mx-[3.5rem] divide-[#DEDEDE] divide-[2px] divide-y text-[#6F6F6F] text-2xl">
+        <div className="mt-[2.5rem] mx-[2.5rem] divide-[#DEDEDE] divide-[2px] divide-y text-[#6F6F6F] text-xl">
           <div>
-            <div className="flex my-[2.5rem] pointer">
+            <div className="flex my-[2rem] pointer">
               <div>
                 <Image src={dashboard} alt="dashboard" />
               </div>
@@ -218,7 +218,7 @@ const page = () => {
                 <p className="text-[#F15B25] font-bold">Dashboard</p>
               </div>
             </div>
-            <div className="flex my-[2.5rem] pointer">
+            <div className="flex my-[2rem] pointer">
               <div>
                 <Image src={Exclude} alt="Exclude" />
               </div>
@@ -226,7 +226,7 @@ const page = () => {
                 <p>Live orders</p>
               </div>
             </div>
-            <div className="flex my-[2.5rem] pointer">
+            <div className="flex my-[2rem] pointer">
               <div>
                 <Image src={Shape} alt="Shape" />
               </div>
@@ -236,7 +236,7 @@ const page = () => {
             </div>
           </div>
           <div>
-            <div className="flex my-[2.5rem] pointer">
+            <div className="flex my-[2rem] pointer">
               <div>
                 <Image src={interests} alt="interests" />
               </div>
@@ -247,7 +247,7 @@ const page = () => {
                 <Image src={arrow_forward_ios} alt="arrow_forward_ios" />
               </div>
             </div>
-            <div className="flex my-[2.5rem] pointer">
+            <div className="flex my-[2rem] pointer">
               <div>
                 <Image src={restaurant_menu} alt="restaurant_menu" />
               </div>
@@ -257,7 +257,7 @@ const page = () => {
             </div>
           </div>
           <div>
-            <div className="flex my-[2.5rem] pointer">
+            <div className="flex my-[2rem] pointer">
               <div>
                 <Image src={switch_account} alt="switch_account" />
               </div>
@@ -265,7 +265,7 @@ const page = () => {
                 <p>Customers</p>
               </div>
             </div>
-            <div className="flex my-[2.5rem] pointer">
+            <div className="flex my-[2rem] pointer">
               <div>
                 <Image src={Exclude1} alt="Exclude1" />
               </div>
@@ -275,7 +275,7 @@ const page = () => {
             </div>
           </div>
           <div>
-            <div className="flex my-[2.5rem] pointer">
+            <div className="flex my-[2rem] pointer">
               <div>
                 <Image src={person} alt="person" />
               </div>
@@ -283,7 +283,7 @@ const page = () => {
                 <p>Edit profile</p>
               </div>
             </div>
-            <div className="flex my-[2.5rem] pointer">
+            <div className="flex my-[2rem] pointer">
               <div>
                 <Image src={logout} alt="logout" />
               </div>
@@ -294,40 +294,40 @@ const page = () => {
           </div>
         </div>
       </div>
-      <div className="mt-[3rem] ms-[3rem]">
-        <div className="my-[1rem] flex text-2xl">
+      <div className="mt-[2rem] ms-[2rem]">
+        <div className="my-[1rem] flex text-xl">
           <div className="relative group">
-            <button className="bg-[#FFFFFF] hover:bg-[#F15B2525] hover:border-[#F15B25] border text-3xl font-bold h-[89px] w-[319px] rounded-xl">
+            <button className="bg-[#FFFFFF] hover:bg-[#F15B2525] hover:border-[#F15B25] border text-2xl font-bold h-[70px] w-[270px] rounded-xl">
               All Brands
             </button>
-            <div className="hidden group-hover:block triangle ms-[150px]"></div>
+            <div className="hidden group-hover:block triangle ms-[125px]"></div>
           </div>
           <div className="relative group h-fit">
-            <button className="flex bg-[#FFFFFF] hover:bg-[#F15B2525] hover:border-[#F15B25] items-center border font-bold h-[89px] w-[319px] rounded-xl ms-[1.5rem]">
+            <button className="flex bg-[#FFFFFF] hover:bg-[#F15B2525] hover:border-[#F15B25] items-center border font-bold h-[70px] w-[270px] rounded-xl ms-[1.5rem]">
               <div className="me-[1rem] ms-[1.5rem]">
                 <Image src={Group131} alt="onnow" />
               </div>
               <p>Party Pizza</p>
             </button>
-            <div className="hidden group-hover:block triangle ms-[165px]"></div>
+            <div className="hidden group-hover:block triangle ms-[140px]"></div>
           </div>
           <div className="relative group h-fit">
-            <button className="flex bg-[#FFFFFF] hover:bg-[#F15B2525] hover:border-[#F15B25] items-center border font-bold h-[89px] w-[319px] rounded-xl ms-[1.5rem]">
+            <button className="flex bg-[#FFFFFF] hover:bg-[#F15B2525] hover:border-[#F15B25] items-center border font-bold h-[70px] w-[270px] rounded-xl ms-[1.5rem]">
               <div className="me-[1rem] ms-[1.5rem]">
                 <Image src={Group132} alt="onnow" />
               </div>
               <p>Wrappo</p>
             </button>
-            <div className="hidden group-hover:block triangle ms-[165px]"></div>
+            <div className="hidden group-hover:block triangle ms-[140px]"></div>
           </div>
           <div className="relative group h-fit">
-            <button className="flex bg-[#FFFFFF] hover:bg-[#F15B2525] hover:border-[#F15B25] items-center border font-bold h-[89px] w-[319px] rounded-xl ms-[1.5rem]">
+            <button className="flex bg-[#FFFFFF] hover:bg-[#F15B2525] hover:border-[#F15B25] items-center border font-bold h-[70px] w-[270px] rounded-xl ms-[1.5rem]">
               <div className="me-[1rem] ms-[1.5rem]">
                 <Image src={Group133} alt="onnow" />
               </div>
               <p>Snackmate</p>
             </button>
-            <div className="hidden group-hover:block triangle ms-[165px]"></div>
+            <div className="hidden group-hover:block triangle ms-[140px]"></div>
           </div>
           <div className="mt-[1.5rem]">
             <button className="ms-[1rem] bg-[#888888] hover:bg-[#F15B25] rounded-full p-[0.2rem]">
@@ -335,33 +335,33 @@ const page = () => {
             </button>
           </div>
         </div>
-        <div className="flex divide-x divide-[#B0B0B0] bg-[#FFFFFF] rounded-xl text-2xl text-[#979797] mt-[3rem] p-[1rem]">
-          <div className="w-[20%] p-[1rem]">
+        <div className="flex divide-x divide-[#B0B0B0] bg-[#FFFFFF] rounded-xl text-xl text-[#979797] mt-[3rem] p-[1rem]">
+          <div className="w-[20%] p-[0.5rem]">
             <h1>Total orders</h1>
-            <p className="text-[#F15B25] text-4xl font-black mt-[0.5rem]">
+            <p className="text-[#F15B25] text-3xl font-black mt-[0.5rem]">
               136
             </p>
           </div>
-          <div className="w-[26%] flex justify-center p-[1rem]">
+          <div className="w-[26%] flex justify-center p-[0.5rem]">
             <div>
               <h1>Total sales</h1>
-              <p className="text-[#F15B25] text-4xl font-black mt-[0.5rem]">
+              <p className="text-[#F15B25] text-3xl font-black mt-[0.5rem]">
                 32652৳
               </p>
             </div>
           </div>
-          <div className="w-[26%] flex justify-center p-[1rem]">
+          <div className="w-[26%] flex justify-center p-[0.5rem]">
             <div>
               <h1>Dispatched Order</h1>
-              <p className="text-[#F15B25] text-4xl font-black mt-[0.5rem]">
+              <p className="text-[#F15B25] text-3xl font-black mt-[0.5rem]">
                 32652৳
               </p>
             </div>
           </div>
-          <div className="w-[26%] flex justify-end p-[1rem]">
+          <div className="w-[26%] flex justify-end p-[0.5rem]">
             <div>
               <h1>Avg. basket value</h1>
-              <p className="text-[#F15B25] text-4xl font-black mt-[0.5rem]">
+              <p className="text-[#F15B25] text-3xl font-black mt-[0.5rem]">
                 370৳
               </p>
             </div>
@@ -373,9 +373,9 @@ const page = () => {
             <div>
               <div className="flex items-center">
                 <div className="grow ms-[2rem]">
-                  <h1 className="text-2xl font-bold">Total orders by source</h1>
+                  <h1 className="text-xl font-bold">Total orders by source</h1>
                 </div>
-                <button className="flex items-center border rounded-full px-[1rem]">
+                <button className="text-sm flex items-center border rounded-full px-[1rem]">
                   January{" "}
                   <Image
                     className="mx-auto ms-[1rem] py-[1rem]"
@@ -383,7 +383,7 @@ const page = () => {
                     alt="Vector632"
                   />
                 </button>
-                <button className="flex items-center border rounded-full px-[1rem] ms-[1rem]">
+                <button className="text-sm flex items-center border rounded-full px-[1rem] ms-[1rem]">
                   Select an outlet{" "}
                   <Image
                     className="mx-auto ms-[1rem] py-[1rem]"
@@ -396,24 +396,24 @@ const page = () => {
                 style={{
                   marginLeft: "30px",
                   height: "3px",
-                  width: "180px",
+                  width: "150px",
                   backgroundColor: "#ff7043",
                   marginBottom: "20px",
                 }}
               ></div>
               <div>
-                <div className="flex font-[600] items-center px-[1rem] ms-[1rem]">
-                  <div className="bg-[#7BB0FF] w-[1.5rem] h-[1.5rem]"></div>
+                <div className="flex font-[600] text-sm items-center px-[1rem] ms-[1rem]">
+                  <div className="bg-[#7BB0FF] w-[1rem] h-[1rem]"></div>
                   <p className="text-[#7BB0FF] ms-[.5rem]">Web</p>
-                  <div className="bg-[#FF569D] w-[1.5rem] h-[1.5rem] ms-[1rem]"></div>
+                  <div className="bg-[#FF569D] w-[1rem] h-[1rem] ms-[1rem]"></div>
                   <p className="text-[#FF569D] ms-[.5rem]">Mobile</p>
                 </div>
               </div>
             </div>
             <div className="pt-[1rem]">
               <LineChart
-                width={650}
-                height={150}
+                width={550}
+                height={120}
                 margin={{
                   top: 20,
                   right: 30,
@@ -471,32 +471,32 @@ const page = () => {
             </div>
           </div>
           <div className="relative bg-[#FFFFFF] rounded-xl ms-[1rem] p-[1rem] ps-[2rem]">
-            <div className="w-[700px] h-[220px]">
-              <h2 className="text-2xl font-bold">Total orders by outlet</h2>
+            <div className="w-[550px] h-[200px]">
+              <h2 className="text-xl font-bold">Total orders by outlet</h2>
               <div
                 style={{
                   height: "3px",
-                  width: "180px",
+                  width: "150px",
                   backgroundColor: "#ff7043",
                   marginBottom: "20px",
                 }}
               ></div>
               <Bar data={data} options={options} />
             </div>
-            <div className="absolute top-[3rem] right-[3rem]">
+            <div className="absolute top-[2rem] right-[2.5rem]">
               <Image
-                className="w-[15px] pointer rotate-180"
+                className="w-[12px] pointer rotate-180"
                 src={Vector632}
                 alt="Vector632"
               />
               <Image
-                className="mt-[10px] w-[15px] pointer"
+                className="mt-[10px] w-[12px] pointer"
                 src={Vector632}
                 alt="Vector632"
               />
             </div>
 
-            <div className="absolute bg-[#BEBEBE] top-[1.5rem] right-[1.5rem] w-[8px] h-[230px] rounded-full">
+            <div className="absolute bg-[#BEBEBE] top-[1rem] right-[1rem] w-[8px] h-[200px] rounded-full">
               <div className="w-full bg-[#6F6F6F] h-[80px] rounded-full pointer"></div>
             </div>
           </div>
@@ -509,19 +509,19 @@ const page = () => {
             padding: "20px",
           }}
         >
-          <h2 className="text-2xl font-bold">
+          <h2 className="text-xl font-bold">
             New customers X Returning customers
           </h2>
           <div
             style={{
               height: "3px",
-              width: "180px",
+              width: "150px",
               backgroundColor: "#ff7043",
               marginBottom: "20px",
             }}
           ></div>
           <table
-            className="table-auto"
+            className="table-auto text-sm"
             style={{
               width: "100%",
               borderCollapse: "collapse",
